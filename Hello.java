a52a3616c731450f45e719de653881d633e583d9
Jyoshika Kandregula <139410324+JyoshikaKandregula@users.noreply.github.com>
2025-01-03 10:58:48 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello{
   public static void main(string args[]){
-    System.out.println("Hello world,this is devops external lab")
+    System.out.println("Hello world,this is devops external lab");
        }
     }
 
